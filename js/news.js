3914f02c2c1d95f17ff84686f9e55279dcf19d2f
Qingyu216 <m18911615202@163.com>
2017-10-21 14:11:07 +0800

    dongxian

---
+++ js/news.js
@@ -0,0 +1,9 @@
+$(function(){
+	/*$(".third_main").mouseover(function(){
+		$(this).find(".third_main_right>p:first-child").css({color:"#fff"});
+		
+	});
+	$(".third_main").mouseout(function(){
+		$(this).find(".third_main_right>p:first-child").css({color:"#767676"});
+	});*/
+});
